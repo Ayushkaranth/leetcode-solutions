a50b2b08fe6e40ce6397b70d4adbd06beb91eb89
ayush karanth <160300874+Ayushkaranth@users.noreply.github.com>
2025-12-07 12:18:15 +0530

    Time: 18 ms (15.04%) | Memory: 46.9 MB (7.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,37 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        char [] a=s.toCharArray();
-        char [] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        for(int i=0;i<a.length;i++){
-            if(a[i]!=b[i]) return false;
+     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {return false;}
+        HashMap<Character,Integer> mapA = new HashMap<>();
+        HashMap<Character,Integer> mapB = new HashMap<>();
+
+        //for s and a
+        for(int i=0;i<s.length();i++){
+            char key = s.charAt(i);
+            if(mapA.containsKey(key)){
+                int freq = mapA.get(key);
+                mapA.put(key,freq+1);
+        }
+            else{
+                mapA.put(key,1);
+            }
+        }
+
+        for(int i=0;i<t.length();i++){
+            char key = t.charAt(i);
+            if(mapB.containsKey(key)){
+                int freq = mapB.get(key);
+                mapB.put(key,freq+1);
+            }
+            else{
+                mapB.put(key,1);
+            }
+        }
+
+        for(char key : mapA.keySet()){
+            int val1 = mapA.get(key);
+            if(!mapB.containsKey(key)){return false;}
+            int val2 = mapB.get(key);
+            if(val1!=val2){return false;}
         }
         return true;
     }
